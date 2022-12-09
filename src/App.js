cf8ecc9f42f1f11c7bb3170d436ccc6611f7f795
Darric Heng <77005864+darricheng@users.noreply.github.com>
2022-12-09 23:42:03 +0800

    Install Ant Design and React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Button } from "antd";
+import "antd/dist/reset.css";
+
 function App() {
-  return;
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
 }
 
 export default App;
