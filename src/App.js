88565a0b11b7f249d9aa936087228b266f4d3d51
Darric Heng <77005864+darricheng@users.noreply.github.com>
2022-12-10 14:54:26 +0800

    Set up layout structure for the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import { Button } from "antd";
 import "antd/dist/reset.css";
+import { Layout, Menu, theme } from "antd";
+const { Header, Content, Footer } = Layout;
 
 function App() {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <div className="App">
-      <Button type="primary">Button</Button>
-    </div>
+    <>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]} />
+        </Header>
+        <Content style={{ padding: "0 50px" }}>
+          <div
+            className="site-layout-content"
+            style={{ background: colorBgContainer }}
+          >
+            Content
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          <p>Copyright © 2022 Darric Heng & Sheikh Mushahid</p>
+          <p>All Rights Reserved</p>
+        </Footer>
+      </Layout>
+    </>
   );
 }
 
