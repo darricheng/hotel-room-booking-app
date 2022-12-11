ffdbdfe809ff718b8888435887104c65316e149f
Darric Heng <77005864+darricheng@users.noreply.github.com>
2022-12-11 16:32:33 +0800

    Flesh out home page with date picker

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,16 +1,20 @@
 // Module imports
-import { DatePicker } from "antd";
-import dayjs from "dayjs";
 import { useState, useEffect } from "react";
+import { DatePicker, Button, Tooltip, Space } from "antd";
+import dayjs from "dayjs";
 
 // Asset imports
 import homePageBanner from "../assets/hotel-home-page-banner.jpg";
+import { SearchOutlined } from "@ant-design/icons";
+import HotelLogoSvg from "../assets/hotel-82-logo-white.js";
 
+// Necessary import inits
 dayjs().format();
-
 const { RangePicker } = DatePicker;
 
+// Styles
 const bannerImageStyle = {
+  position: "relative",
   textAlign: "center",
   // Width and height are set to 100vw and 40vw so that the image always appears as the intended size
   // regardless of the screen size.
@@ -18,6 +22,21 @@ const bannerImageStyle = {
   width: "100vw",
   height: "40vw",
   backgroundImage: `url(${homePageBanner})`,
+  backgroundSize: "cover",
+};
+const mainHotelLogoStyle = {
+  position: "absolute",
+  top: "4vw",
+  left: "0",
+  right: "0",
+  margin: "0 auto",
+};
+const dateRangeStyle = {
+  position: "absolute",
+  bottom: "4vw",
+  left: "0",
+  right: "0",
+  margin: "0 auto",
 };
 
 // Initialise a default date range with the current date and the next day.
@@ -43,13 +62,25 @@ export default function HomePage() {
   };
 
   return (
-    <div className="site-homepage-content" style={bannerImageStyle}>
-      <h1>Hotel 82</h1>
-      <RangePicker
-        size="large"
-        defaultValue={[dayjs(), dayjs().add(1, "day")]}
-        onChange={handleDateChange}
-      />
+    <div className="site-homepage-banner-content" style={bannerImageStyle}>
+      <div className="site-homepage-banner-title" style={mainHotelLogoStyle}>
+        <HotelLogoSvg sizeMultiplier={8} />
+      </div>
+      <div className="intra-banner-content" style={dateRangeStyle}>
+        {/* Set the range picker and button to be side-by-side */}
+        <Space align="end">
+          <RangePicker
+            size="large"
+            defaultValue={[dayjs(), dayjs().add(1, "day")]}
+            onChange={handleDateChange}
+          />
+          <Tooltip title="Find Rooms">
+            <Button type="primary" icon={<SearchOutlined />} size="large">
+              Find Rooms
+            </Button>
+          </Tooltip>
+        </Space>
+      </div>
     </div>
   );
 }
