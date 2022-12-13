ded297075f57b7945381e83d4520491e698319a2
Darric Heng <77005864+darricheng@users.noreply.github.com>
2022-12-13 21:17:43 +0800

    Hide login page if user is logged in

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -2,10 +2,15 @@
 
 // Ant Design imports
 import { Button, Checkbox, Form, Input, Typography } from "antd";
+
+// Module imports
+import { useContext } from "react";
+
 // Firebase imports
 // See https://firebase.google.com/docs/auth/web/start#sign_up_new_users
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase/firebaseConfig";
+import { AuthContext } from "../firebase/AuthContext";
 
 const LoginPageStyle = {
   display: "flex",
@@ -16,6 +21,10 @@ const LoginPageStyle = {
 };
 
 export default function LoginPage() {
+  // Check if the user is logged in
+  const { user } = useContext(AuthContext);
+
+  // Handle the form submission
   const onFinish = (values) => {
     console.log("Success:", values);
 
@@ -42,70 +51,75 @@ export default function LoginPage() {
   return (
     <div style={LoginPageStyle}>
       <Typography.Title>Login</Typography.Title>
-      <Form
-        name="basic"
-        labelCol={{
-          span: 8,
-        }}
-        wrapperCol={{
-          span: 16,
-        }}
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[
-            {
-              type: "email",
-              required: true,
-              message: "Please input your email",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your password",
-            },
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item
-          name="remember"
-          valuePropName="checked"
+      {/* If the user is already logged in, don't show the form (they aren't supposed to be at this page anyway!) */}
+      {user ? (
+        <Typography.Paragraph>You are already logged in!</Typography.Paragraph>
+      ) : (
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
           wrapperCol={{
-            offset: 8,
             span: 16,
           }}
-        >
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item
-          wrapperCol={{
-            offset: 8,
-            span: 16,
+          initialValues={{
+            remember: true,
           }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
         >
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                type: "email",
+                required: true,
+                message: "Please input your email",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      )}
     </div>
   );
 }
