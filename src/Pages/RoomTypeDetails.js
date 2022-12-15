c10f30d06df5d9e0ed69d5318b31f91e535fe459
Darric Heng <77005864+darricheng@users.noreply.github.com>
2022-12-15 21:41:41 +0800

    Room details page gets list of room by type

--- src/Pages/RoomTypeDetails.js
+++ src/Pages/RoomTypeDetails.js
@@ -1,27 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
-const getRoomsFromApi = async (roomType) => {
-  try {
-    const response = await fetch(
-      `${process.env.REACT_APP_API_URL}/rooms?room_type=${roomType}`
-    );
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 export default function RoomTypeDetails() {
+  // State to store the available rooms returned from the api call
+  const [rooms, setRooms] = useState([]);
+
   const { roomType } = useParams();
   useEffect(() => {
+    const getRoomsFromApi = async (roomType) => {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_URL}/rooms?room_type=${roomType}`
+        );
+        const rooms = await response.json();
+        setRooms(rooms);
+      } catch (error) {
+        console.error(error);
+      }
+    };
     getRoomsFromApi(roomType);
   }, [roomType]);
 
   return (
     <div>
       <h1>Room Type Details: {roomType}</h1>
+      {rooms.every((room) => room.booked) ? (
+        <p>No rooms available</p>
+      ) : (
+        <p>Rooms available</p>
+      )}
     </div>
   );
 }
