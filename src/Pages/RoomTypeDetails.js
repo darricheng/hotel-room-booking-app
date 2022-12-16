468f66ad03945a2dfc7b95c3d40a6ce9b62b1a1b
Darric Heng <77005864+darricheng@users.noreply.github.com>
2022-12-16 21:56:49 +0800

    Update state management for room type details page

--- src/Pages/RoomTypeDetails.js
+++ src/Pages/RoomTypeDetails.js
@@ -3,12 +3,27 @@ import { useParams } from "react-router";
 
 // TODO: use dayjs().toDate() to convert the date strings to date objects when calling the book room api
 
-export default function RoomTypeDetails() {
+export default function RoomTypeDetails(props) {
+  // Get the room search setting from App state
+  // setRoomSearchSetting is for setting the single source of truth for the user's selected dates and room type
+  const { roomSearchSetting, setRoomSearchSetting } = props;
+
   // State to store the available rooms returned from the api call
   const [rooms, setRooms] = useState([]);
 
+  // Get the room type from the url instead of App state because the user may directly access the room type details page
   const { roomType } = useParams();
   useEffect(() => {
+    // Set the room type in App state to the room type in the url
+    setRoomSearchSetting({
+      ...roomSearchSetting,
+      roomType: roomType,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [roomType]); // Only want the effect to run when the room type changes
+
+  useEffect(() => {
+    // Function to get the available rooms from the api
     const getRoomsFromApi = async (roomType) => {
       try {
         const response = await fetch(
